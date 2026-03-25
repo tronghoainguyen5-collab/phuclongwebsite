526191b6b290c85f5698ec81c8ec6068df88c0d6
tronghoainguyen5-collab <tronghoainguyen5@gmail.com>
2026-03-26 01:34:03 +0700

    Delete services directory

--- services/CategoryService.d.ts
+++
@@ -1,6 +0,0 @@
-import { Category } from "../models/Category.js";
-import { ApiService } from "./ApiService.js";
-export declare class CategoryService extends ApiService {
-    getAll(): Promise<Category[]>;
-}
-//# sourceMappingURL=CategoryService.d.ts.map
--- services/CategoryService.js
+++
@@ -1,9 +0,0 @@
-import { Category } from "../models/Category.js";
-import { ApiService } from "./ApiService.js";
-export class CategoryService extends ApiService {
-    async getAll() {
-        const data = await this.get("categories");
-        return data.map((c) => new Category(c.id, c.name));
-    }
-}
-//# sourceMappingURL=CategoryService.js.map
